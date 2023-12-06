e346392edbee6dd2fe594391f4ec9112062bce8e
imperator1440 <imperator1440@gmail.com>
2023-12-06 16:44:43 +0300

    add simple error handling to places routes

--- src/routes/places.routes.js
+++ src/routes/places.routes.js
@@ -18,18 +18,32 @@ const DUMMY_PLACES = [
 
 router.get('/:pid', (req, res, next) => {
   const placeId = req.params.pid;
+  
   const place = DUMMY_PLACES.find(place => {
     return place.id === placeId;
   });
+
+  if (!place) {
+    const error = new Error('Could not find a place with provided place id!');
+    error.code = 404;
+    throw error;
+  }
 
   res.json({ place });
 });
 
 router.get('/user/:uid', (req, res, next) => {
   const userId = req.params.uid;
+
   const place = DUMMY_PLACES.find(place => {
     return place.creator === userId;
   });
+
+  if (!place) {
+    const error = new Error('Could not find a place with provided place id!');
+    error.code = 404;
+    return next(error);
+  }
   
   res.json({ place });
 });
