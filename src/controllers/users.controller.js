360af85c3e99b4a18996281e31fa4f1c0de6e5d5
imperator1440 <imperator1440@gmail.com>
2024-01-15 17:53:01 +0300

    add logging with mongodb and mongoose

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -20,7 +20,7 @@ const signup = async (req, res, next) => {
     return next(error);
   }
 
-  const { name, email, password } = req.body;
+  const { name, email, password, places } = req.body;
 
   let existingUser;
   try {
@@ -53,12 +53,20 @@ const signup = async (req, res, next) => {
   res.status(201).json({ user: createdUser.toObject({ getters: true })});
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   const { email, password } = req.body;
   
-  const identifiedUser = DUMMY_USERS.find(user => user.email === email);
-  if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError('Could not identify user, credentials seem to be wrong.', 401);
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch(err) {
+    const error = new HttpError('Loggin in failed, please try again later.', 500);
+    return next(error);
+  }
+
+  if (!existingUser || existingUser.password !== password) {
+    const error = new HttpError('Invalid credentials, could not log you in.', 401);
+    return next(error);
   }
 
   res.json({ message: 'Logged in!' });
